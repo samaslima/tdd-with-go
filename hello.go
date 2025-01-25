532660ccbf618c6ac8fdbd5ac1b67198e5efacfd
samara lima <samara@samaras-MacBook-Pro.local>
2025-01-25 15:30:42 -0300

    Refactor 'hello world'

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "hello "
+
 func Hello(name string) string {
-	return "hello " + name
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
