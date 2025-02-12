a767ee978f58c931572f09b26ab2f4e9d64daae9
samaslima <samaslimag@gmail.com>
2025-02-12 20:54:48 -0300

    Improve tests

--- hello_test.go
+++ hello_test.go
@@ -4,13 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("samara")
+		got := Hello("samara", "english")
 		want := "hello samara"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'hello world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "english")
 		want := "hello world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("elodie", "spanish")
+		want := "hola elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("mary", "french")
+		want := "bonjour mary"
 		assertCorrectMessage(t, got, want)
 	})
 }
