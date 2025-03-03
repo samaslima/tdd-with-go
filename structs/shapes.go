3dd93e2feefefc6e28b776eb760d3986aa4f609c
samaslima <samaslimag@gmail.com>
2025-03-03 14:04:19 -0300

    Add Triangle struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
